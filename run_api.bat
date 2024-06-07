b2a55b37964a313545f7684e1b839472197e9db1
nefko <nm53229@fer.hr>
2024-06-07 02:19:47 +0200

    * docker additions

--- run_api.bat
+++ run_api.bat
@@ -18,7 +18,7 @@ if %errorlevel% neq 0 (
 )
 
 echo Running Docker container...
-docker run -d -p 8000:8000 my-fastapi-app
+docker run -d -p 8000:8000 --name my-fastapi-container my-fastapi-app
 
 if %errorlevel% neq 0 (
     echo Failed to run Docker container.
@@ -27,4 +27,14 @@ if %errorlevel% neq 0 (
 )
 
 echo Docker container is running and FastAPI app should be accessible at http://localhost:8000
+echo Press any key to stop the Docker container and exit.
+
+:: Wait for any key press
+pause
+
+echo Stopping Docker container...
+docker stop my-fastapi-container
+docker rm my-fastapi-container
+
+echo Docker container stopped.
 pause
